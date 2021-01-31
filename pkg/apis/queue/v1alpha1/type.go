edd778b87d81743f2ad0911578054c31baa690f3
lucaswzhang <lucaswzhang@tencent.com>
2021-01-31 12:42:42 +0800

    update proto file generated

--- pkg/apis/queue/v1alpha1/type.go
+++ pkg/apis/queue/v1alpha1/type.go
@@ -10,9 +10,9 @@ import (
 
 type QueueUnit struct {
 	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata"`
-	Spec              Spec   `json:"spec"`
-	Status            Status `json:"status,omitempty"`
+	metav1.ObjectMeta `json:"metadata" protobuf:"bytes,1,name=metadata"`
+	Spec              Spec   `json:"spec" protobuf:"bytes,2,name=spec"`
+	Status            Status `json:"status,omitempty" protobuf:"bytes,3,opt,name=status"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -23,11 +23,14 @@ type QueueUnitList struct {
 	Items           []QueueUnit `json:"items"`
 }
 
+const DefaultPriority int32 = 999
+const DefaultQueueName string = "default"
+
 type Spec struct {
-	JobType  string              `json:"type"`
-	Priority int32               `json:"priority"`
-	Queue    string              `json:"queue"`
-	Resource corev1.ResourceList `json:"resource"`
+	JobType  string              `json:"type" protobuf:"bytes,1,name=jobType"`
+	Priority int32               `json:"priority,omitempty" protobuf:"varint,2,opt,name=priority"`
+	Queue    string              `json:"queue,omitempty" protobuf:"bytes,3,opt,name=queue"`
+	Resource corev1.ResourceList `json:"resource" protobuf:"bytes,4,name=resource"`
 }
 
 type JobPhase string
@@ -38,6 +41,6 @@ const (
 )
 
 type Status struct {
-	Phase   JobPhase `json:"phase"`
-	Message string   `json:"message,omitempty"`
+	Phase   JobPhase `json:"phase" protobuf:"bytes,1,name=phase"`
+	Message string   `json:"message,omitempty" protobuf:"bytes,2,opt,name=message"`
 }
